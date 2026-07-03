c779f87263075b6a8affd4ea8c41bccfbf789cdc
DTN <123billbao123@gmail.com>
2026-07-03 12:04:59 +0700

    refactor: move di.dart to core/di/ folder for consistency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'core/di.dart';
+import 'core/di/di.dart';
 import 'firebase_options.dart';
 
 void main() async {
